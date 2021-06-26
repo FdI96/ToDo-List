8cf35d8e2a79c95ee4489020d15f033959a3ece7
FdI96 <federicolopezce@gmail.com>
2021-06-26 18:46:39 -0300

    Fix delete issue

--- src/toDo.js
+++ src/toDo.js
@@ -7,79 +7,81 @@ class ToDo {
     if (id) {
       this.id = id;
     } else {
-      let acc = localStorage.getItem('accToDo');
+      let acc = localStorage.getItem("accToDo");
       acc = Number(acc);
       acc += 1;
       this.id = acc;
-      localStorage.setItem('accToDo', acc);
+      localStorage.setItem("accToDo", acc);
     }
   }
 
   removeElement() {
-    const arrayDefault = JSON.parse(localStorage.getItem('Default'));
-    const currentArray = JSON.parse(localStorage.getItem('currentArray'));
+    const arrayDefault = JSON.parse(localStorage.getItem("Default"));
+    const currentArray = JSON.parse(localStorage.getItem("currentArray"));
     const findById = (element) => element.id === this.id;
     const indexDefault = arrayDefault.findIndex(findById);
     const indexCA = currentArray.findIndex(findById);
     arrayDefault.splice(indexDefault, 1);
-    localStorage.setItem('Default', JSON.stringify(arrayDefault));
+    localStorage.setItem("Default", JSON.stringify(arrayDefault));
 
     currentArray.splice(indexCA, 1);
-    const currentProject = localStorage.getItem('currentProject');
+    const currentProject = localStorage.getItem("currentProject");
     localStorage.setItem(currentProject, JSON.stringify(currentArray));
 
+    localStorage.setItem("currentArray", currentArray);
+
     const cardToRemove = document.getElementById(`${this.id}`);
-    const showToDoList = document.getElementById('toDoListShow');
+    const showToDoList = document.getElementById("toDoListShow");
     showToDoList.removeChild(cardToRemove);
   }
 
   print() {
-    const card = document.createElement('div');
+    const card = document.createElement("div");
     let color = null;
-    if (this.priority === 'low') {
-      color = 'primary';
-    } else if (this.priority === 'medium') {
-      color = 'warning';
+    if (this.priority === "low") {
+      color = "primary";
+    } else if (this.priority === "medium") {
+      color = "warning";
     } else {
-      color = 'danger';
+      color = "danger";
     }
-    card.setAttribute('class', `card text-white bg-${color}`);
-    card.setAttribute('id', `${this.id}`);
+    card.setAttribute("class", `card text-white bg-${color}`);
+    card.setAttribute("id", `${this.id}`);
 
-    const title = document.createElement('div');
-    title.setAttribute('class', 'card-header text-white');
+    const title = document.createElement("div");
+    title.setAttribute("class", "card-header text-white");
     title.innerHTML = this.title;
     card.appendChild(title);
 
-    const body = document.createElement('div');
-    body.setAttribute('class', 'card-body text-white');
+    const body = document.createElement("div");
+    body.setAttribute("class", "card-body text-white");
     card.appendChild(body);
 
-    const bodyTitle = document.createElement('h5');
-    bodyTitle.setAttribute('class', 'card-title text-white');
+    const bodyTitle = document.createElement("h5");
+    bodyTitle.setAttribute("class", "card-title text-white");
     bodyTitle.innerHTML = this.description;
     body.appendChild(bodyTitle);
 
-    let bodyP = document.createElement('p');
-    bodyP.setAttribute('class', 'card-text text-white');
+    let bodyP = document.createElement("p");
+    bodyP.setAttribute("class", "card-text text-white");
     bodyP.innerHTML = this.date;
     body.appendChild(bodyP);
 
-    bodyP = document.createElement('p');
-    bodyP.setAttribute('class', 'card-text text-white');
+    bodyP = document.createElement("p");
+    bodyP.setAttribute("class", "card-text text-white");
     bodyP.innerHTML = this.priority;
     body.appendChild(bodyP);
 
-    const keypad = document.createElement('div');
-    keypad.setAttribute('class', 'row container');
+    const keypad = document.createElement("div");
+    keypad.setAttribute("class", "row container");
     card.appendChild(keypad);
 
-    const delButton = document.createElement('button');
-    delButton.setAttribute('id', 'deleteButton');
-    delButton.setAttribute('class', 'col-2');
-    delButton.innerHTML = 'Delete';
+    const delButton = document.createElement("button");
+    delButton.setAttribute("id", "deleteButton");
+    delButton.setAttribute("class", "col-2");
+    delButton.innerHTML = "Delete";
     keypad.appendChild(delButton);
-    delButton.addEventListener('click', () => this.removeElement());
+    delButton.addEventListener("click", () => this.removeElement());
 
     return card;
   }
