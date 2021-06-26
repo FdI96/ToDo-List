eebac7d27ac2c6b11be7766db8b7681f87b54876
FdI96 <federicolopezce@gmail.com>
2021-06-26 19:19:11 -0300

    Fix linters

--- src/projects.js
+++ src/projects.js
@@ -1,50 +1,49 @@
-import addButtonComponent from "./addButton";
-import ToDo from "./toDo";
+import addButtonComponent from './addButton';
+import ToDo from './toDo';
 
-let projects = JSON.parse(localStorage.getItem("projectsArray"));
-projects = !projects ? ["Default"] : projects;
-localStorage.setItem("projectsArray", projects);
+let projects = JSON.parse(localStorage.getItem('projectsArray'));
+projects = !projects ? ['Default'] : projects;
+localStorage.setItem('projectsArray', projects);
 
-let currentP = localStorage.getItem("currentProject");
-currentP ??= localStorage.setItem("currentProject", "Default");
+let currentP = localStorage.getItem('currentProject');
+currentP ??= localStorage.setItem('currentProject', 'Default');
 
-let arrayCurrent = JSON.parse(localStorage.getItem("Default"));
+let arrayCurrent = JSON.parse(localStorage.getItem('Default'));
 arrayCurrent ??= [];
 
 const projectListComponent = () => {
-  const projectList = document.getElementById("projectList");
+  const projectList = document.getElementById('projectList');
 
-  const divProjectsButton = document.createElement("div");
-  divProjectsButton.setAttribute("class", "row");
+  const divProjectsButton = document.createElement('div');
+  divProjectsButton.setAttribute('class', 'row');
 
-  const title = document.createElement("h1");
-  title.setAttribute("class", "col-8");
-  title.innerHTML = "Projects";
+  const title = document.createElement('h1');
+  title.setAttribute('class', 'col-8');
+  title.innerHTML = 'Projects';
   divProjectsButton.appendChild(title);
   const form = addButtonComponent();
   divProjectsButton.appendChild(form);
 
-  const uList = document.createElement("ul");
+  const uList = document.createElement('ul');
 
   for (let index = 0; index < projects.length; index += 1) {
-    const li = document.createElement("li");
-    const button = document.createElement("button");
+    const li = document.createElement('li');
+    const button = document.createElement('button');
     button.innerHTML = projects[index];
-    button.addEventListener("click", (e) => {
-      localStorage.setItem("currentProject", e.target.innerHTML);
+    button.addEventListener('click', (e) => {
+      localStorage.setItem('currentProject', e.target.innerHTML);
       const targetValue = e.target.innerHTML;
       const getValue = localStorage.getItem(targetValue);
       const arrayCurrent = JSON.parse(`${getValue}`);
 
-      localStorage.setItem("currentArray", JSON.stringify(arrayCurrent));
-      const list = document.getElementById("toDoListShow");
+      localStorage.setItem('currentArray', JSON.stringify(arrayCurrent));
+      const list = document.getElementById('toDoListShow');
 
       list.innerHTML = null;
-      const currentP = localStorage.getItem("currentProject");
-      const arrayAux =
-        currentP === "Default"
-          ? JSON.parse(localStorage.getItem("Default"))
-          : JSON.parse(localStorage.getItem("currentArray"));
+      const currentP = localStorage.getItem('currentProject');
+      const arrayAux = currentP === 'Default'
+        ? JSON.parse(localStorage.getItem('Default'))
+        : JSON.parse(localStorage.getItem('currentArray'));
 
       for (let index = 0; index < arrayAux.length; index += 1) {
         const card = new ToDo(
@@ -52,13 +51,13 @@ const projectListComponent = () => {
           arrayAux[index].description,
           arrayAux[index].date,
           arrayAux[index].priority,
-          arrayAux[index].id
+          arrayAux[index].id,
         );
 
         if (card.id) {
           const renderCard = card.print();
           list.appendChild(renderCard);
-          const toList = document.getElementById("toDoList");
+          const toList = document.getElementById('toDoList');
           toList.appendChild(list);
         }
       }
@@ -75,17 +74,17 @@ const projectListComponent = () => {
   projectList.appendChild(uList);
 
   const submitButton = form.children[1].children[0];
-  submitButton.addEventListener("click", () => {
+  submitButton.addEventListener('click', () => {
     projects.push(form.children[0].value);
-    const li = document.createElement("li");
-    const button = document.createElement("button");
+    const li = document.createElement('li');
+    const button = document.createElement('button');
     button.innerHTML = form.children[0].value;
-    button.addEventListener("click", (e) => {
-      localStorage.setItem("currentProject", e.target.innerHTML);
-      const item = localStorage.getItem("currentProject");
+    button.addEventListener('click', (e) => {
+      localStorage.setItem('currentProject', e.target.innerHTML);
+      const item = localStorage.getItem('currentProject');
       arrayCurrent = JSON.parse(localStorage.getItem(item));
-      localStorage.setItem("currentArray", JSON.stringify(arrayCurrent));
-      const toDoL = document.getElementById("toDoListShow");
+      localStorage.setItem('currentArray', JSON.stringify(arrayCurrent));
+      const toDoL = document.getElementById('toDoListShow');
       toDoL.innerHTML = null;
       for (let index = 0; index < arrayCurrent.length; index += 1) {
         const card = new ToDo(
@@ -93,24 +92,24 @@ const projectListComponent = () => {
           arrayCurrent[index].description,
           arrayCurrent[index].date,
           arrayCurrent[index].priority,
-          arrayCurrent[index].id
+          arrayCurrent[index].id,
         );
         if (card.id) {
           const renderCard = card.print();
-          const toDoListShow = document.getElementById("toDoListShow");
+          const toDoListShow = document.getElementById('toDoListShow');
           toDoListShow.appendChild(renderCard);
         }
       }
     });
-    localStorage.setItem("currentProject", form.children[0].value);
+    localStorage.setItem('currentProject', form.children[0].value);
     const key = form.children[0].value;
     localStorage.setItem(`${key}`, JSON.stringify([]));
-    localStorage.setItem("currentArray", "[]");
-    const toDoListShow = document.getElementById("toDoListShow");
+    localStorage.setItem('currentArray', '[]');
+    const toDoListShow = document.getElementById('toDoListShow');
     toDoListShow.innerHTML = null;
     li.appendChild(button);
     uList.appendChild(li);
-    localStorage.setItem("projectsArray", JSON.stringify(projects));
+    localStorage.setItem('projectsArray', JSON.stringify(projects));
   });
 
   return projectList;
